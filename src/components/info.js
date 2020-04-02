b307c9764c8d1899a453b79e5a58be136c2b4bdc
Anton Rodionov <stvdent47@gmail.com>
2020-04-03 02:08:54 +0300

    created components

---
+++ src/components/info.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Info extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>weather forecast</h2>
+                <p>find out your weather</p>
+            </div>
+        )
+    };
+}
+
+export default Info;
